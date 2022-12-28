a1e595d5a5b8bd9d9e159c77c62fd675669efb9e
Kenneth Salanga <kennethsalanga@gmail.com>
2022-12-27 19:59:48 -0500

    Initial Node Structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,29 @@
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
 
+#[derive(Debug)]
+struct Node<T> {
+    value: RefCell<T>,
+    parent: RefCell<Weak<Node<T>>>,
+    children: RefCell<Vec<Rc<Node<T>>>>,
+}
+
+impl<T> Node<T> {
+    fn new(value: T) -> Node<T> {
+        Node {
+            value: RefCell::new(value),
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![]),
+        }
+    }
+
+    fn set_parent(&mut self, parent: Rc<Node<T>>) {
+        *self.parent.borrow_mut() = Rc::downgrade(&parent);
+    }
+
+    fn add_child(&mut self, child: Rc<Node<T>>) {
+        let mut children = self.children.borrow_mut();
+
+        children.push(child);
+    }
+}
