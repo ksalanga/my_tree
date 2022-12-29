0a6e8d92d87c042a90d73d6ddad31c52bda6c8ae
Kenneth Salanga <kennethsalanga@gmail.com>
2022-12-29 02:12:02 -0500

    Node add_child method:
    Takes reference of Node self, the address of Reference Count to Self, and the child to add to self.
    
    child parameter makes sense. Reference Count of child now gives up ownership of itself to the parent.
    
    We need the self_ref in the parameters because without the Rc to self, the child cannot create a shared reference of the parent.
    - It needs the address of the Rc to Self to create its own weak reference to Self based on that originating address of Rc to Self.
    - The child's reference to the parent: self cannot simply be a regular reference &self because we'd have to use lifetimes for &self. lifetimes guarantee that the reference to parent will live as long as the child, and while this is the case for our example, it isn't always the case. Also, smart pointers (references) are much more intuitive and safe for shared references.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::ptr;
 use std::rc::{Rc, Weak};
 
 #[derive(Debug)]
@@ -9,19 +10,19 @@ struct Node<T> {
 }
 
 impl<T> Node<T> {
-    fn new(value: T) -> Node<T> {
-        Node {
+    pub fn new(value: T) -> Rc<Node<T>> {
+        Rc::new(Node {
             value: RefCell::new(value),
             parent: RefCell::new(Weak::new()),
             children: RefCell::new(vec![]),
-        }
+        })
     }
 
-    fn set_parent(&mut self, parent: Rc<Node<T>>) {
-        *self.parent.borrow_mut() = Rc::downgrade(&parent);
-    }
+    pub fn add_child(&self, self_ref: &Rc<Self>, child: Rc<Node<T>>) {
+        assert!(ptr::eq(self, self_ref.as_ref()));
 
-    fn add_child(&mut self, child: Rc<Node<T>>) {
+        *child.parent.borrow_mut() = Rc::downgrade(self_ref);
+
         let mut children = self.children.borrow_mut();
 
         children.push(child);
@@ -44,3 +45,48 @@ impl<T> Node<T> {
 //         }
 //     }
 // }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_child() {
+        let a = Node::new(1);
+
+        let b = Node::new(2);
+
+        a.add_child(&a, b);
+
+        assert_eq!(a.children.borrow().len(), 1);
+    }
+
+    #[test]
+    fn add_child_has_parent() {
+        let a = Node::new(1);
+
+        let b = Node::new(2);
+
+        a.add_child(&a, b);
+
+        let a_children = a.children.borrow();
+        let b = a_children.get(0).unwrap();
+
+        let b_parent = b.parent.borrow();
+
+        assert!(b_parent.ptr_eq(&Rc::downgrade(&a)));
+    }
+
+    #[test]
+    #[should_panic]
+    fn add_child_with_child_parent_pointer_to_wrong_parent() {
+        let a = Node::new(1);
+
+        let b = Node::new(2);
+        let c = Node::new(2);
+
+        a.add_child(&c, b);
+
+        assert_eq!(a.children.borrow().len(), 1);
+    }
+}
